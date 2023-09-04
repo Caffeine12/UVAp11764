17c4fe08c92fb354b39787627cbebf3c88a78295
Caffeine12 <jubairku12@gmail.com>
2023-09-04 16:10:46 +0200

    UVA problem 11764- Accepted

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);
         int testCase=sc.nextInt();
+
         for (int i=1;i<=testCase;i++)
         {
+            int highJump=0, lowJump=0;
             int wallNumber=sc.nextInt(); 
-            String blank=sc.nextLine();      //blank is just for calncelling out the new line character from nextInt() method                                 
+            String blank=sc.nextLine();      //blank is just for calncelling out the new line character from previous nextInt() method                                 
             String wallstring=sc.nextLine();
             String[] wallArr=wallstring.split("\\s");
-            int[] wallIntArr=Arrays.stream(wallArr).mapToInt(Integer::parseInt).toArray();                
-            
-            
+            int[] wallIntArr=Arrays.stream(wallArr).mapToInt(Integer::parseInt).toArray();
+            for(int j=0;j<wallNumber-1;j++)
+            {
+                if(wallIntArr[j]<wallIntArr[j+1])
+                {
+                    highJump++;
+                }
+                else if(wallIntArr[j]>wallIntArr[j+1])
+                {
+                    lowJump++;
+                }                
+            }
+            System.out.println("Case "+ i +": "+highJump+" "+lowJump);
         }
     }
 }
